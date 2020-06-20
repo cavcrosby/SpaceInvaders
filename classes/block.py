b8bf7ef28ca8a9361669536a4f1217f24cb4cb2d
Conner Crosby <crosbyco3@gmail.com>
2020-06-20 11:38:57 -0400

    Refactor code

--- classes/block.py
+++ classes/block.py
@@ -5,6 +5,7 @@ from pygame.rect import Rect
 
 # Local Application Imports
 
+
 class Block:
 
     RECTS_PER_COLUMN = 4
@@ -25,24 +26,12 @@ class Block:
         top = top_left[Y_CORD]
         width = abs(bottom_right[X_CORD] - top_left[X_CORD])
         height = abs(bottom_right[Y_CORD] - top_left[Y_CORD])
-        TOP_DECREMENT = height/self.RECTS_PER_COLUMN
-        LEFT_DECREMENT = width/self.RECTS_PER_ROW
+        TOP_DECREMENT = height / self.RECTS_PER_COLUMN
+        LEFT_DECREMENT = width / self.RECTS_PER_ROW
 
         for row_n in range(self.RECTS_PER_ROW):
             top_copy = top
             for col_n in range(self.RECTS_PER_COLUMN):
-                self._structure.append(
-                    Rect(
-                        left,
-                        top_copy,
-                        width,
-                        height,
-                    )
-                )
+                self._structure.append(Rect(left, top_copy, width, height,))
                 top_copy += TOP_DECREMENT
             left += LEFT_DECREMENT
-
-
-b = Block((0,0), (1, 1))
-#print(len(b.STRUCTURE))
-print(b.STRUCTURE[0].y)
